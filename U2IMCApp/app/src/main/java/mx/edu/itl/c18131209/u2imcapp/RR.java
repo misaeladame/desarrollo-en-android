0f8aa36f869e0c06c80b1bb888b06b5d5e33051d
Misael Adame <misael_adame@protonmail.com>
2021-04-19 01:26:01 -0500

    Practica 01 U3

--- U2IMCApp/app/src/main/java/mx/edu/itl/c18131209/u2imcapp/RR.java
+++ U2IMCApp/app/src/main/java/mx/edu/itl/c18131209/u2imcapp/RR.java
@@ -1,3 +1,26 @@
+/*------------------------------------------------------------------------------------------
+:*                         TECNOLOGICO NACIONAL DE MEXICO
+:*                       INSTITUTO TECNOLOGICO DE LA LAGUNA
+:*                     INGENIERIA EN SISTEMAS COMPUTACIONALES
+:*                             DESARROLLO EN ANDROID "A"
+:*
+:*                   SEMESTRE: ENE-JUN/2021    HORA: 10-11 HRS
+:*
+:*                                 Clase Obtener Strings
+:*
+:*  Archivo     : RR.java
+:*  Autor       : Jose Misael Adame Sandoval     18131209
+:*  Fecha       : 16/Abr/2021
+:*  Compilador  : Android Studio 4.1.2
+:*  Descripción : Clase que obtiene los recursos de los strings.xml para la adaptación
+:*                de la aplicación a distintos idiomas.
+:*
+:*  Ultima modif:
+:*  Fecha       Modificó             Motivo
+:*==========================================================================================
+:*
+:*------------------------------------------------------------------------------------------*/
+
 package mx.edu.itl.c18131209.u2imcapp;
 
 import android.content.Context;
