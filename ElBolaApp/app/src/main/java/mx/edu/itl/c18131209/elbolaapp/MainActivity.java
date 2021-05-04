dae6eb8ca7b50772e80bbafbccb26534bbf97759
Misael Adame <misael_adame@protonmail.com>
2021-05-04 10:14:34 -0500

    sE agregan detalles

--- ElBolaApp/app/src/main/java/mx/edu/itl/c18131209/elbolaapp/MainActivity.java
+++ ElBolaApp/app/src/main/java/mx/edu/itl/c18131209/elbolaapp/MainActivity.java
@@ -2,18 +2,61 @@ package mx.edu.itl.c18131209.elbolaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
+
+    private Button btnFiesta;
+    private MediaPlayer mediaPlayer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Asignar la referencia del boton de la party
+        btnFiesta = findViewById ( R.id.btnFiesta );
+        // Registrar este activity como listener de los eventos Touch del boton Party
+        btnFiesta.setOnTouchListener ( this );
+        // Establecer el canal de audio para reproducción de música
+        setVolumeControlStream ( AudioManager.STREAM_MUSIC );
+
     }
 
     public void btnSpotifyClick ( View v ) {
 
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent ) {
+        if ( view.getId() == R.id.btnFiesta ) {
+            switch ( motionEvent.getAction () ) {
+                case MotionEvent.ACTION_DOWN : reproducir_audio ();  break;
+                case MotionEvent.ACTION_UP   : detener_audio ();  break;
+
+            }
+            return true;
+        }
+
+        return false;
+    }
+
+    private void reproducir_audio () {
+        if ( mediaPlayer == null ) {
+            mediaPlayer = MediaPlayer.create ( this, R.raw.tropicalisimo );
+        }
+        mediaPlayer.start ();
+    }
+
+    private void detener_audio () {
+        if ( mediaPlayer != null ) {
+            mediaPlayer.stop ();
+            mediaPlayer = null;
+        }
     }
 }
