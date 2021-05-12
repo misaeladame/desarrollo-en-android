2f60dfaaf2d07b52de47bd90b51a885d9df57a12
Misael Adame <misael_adame@protonmail.com>
2021-05-12 08:05:24 -0500

    App para ver si funcionaba la orientacion

--- U4VideoApp/app/src/main/java/mx/edu/itl/c18131209/u4videoapp/MainActivity.java
+++ U4VideoApp/app/src/main/java/mx/edu/itl/c18131209/u4videoapp/MainActivity.java
@@ -2,13 +2,56 @@ package mx.edu.itl.c18131209.u4videoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String ruta;
+
+    //----------------------------------------------------------------------------------------------
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //----------------------------------------------------------------------------------------------
+
+    public void btnDesdeAppClick ( View v ) {
+        // Reproducir un video incluido en la app en la carpeta raw
+        ruta = "android.resource://" + this.getPackageName() + "/" + R.raw.la_pachanga;
+        lanzarVideoActiviy ();
+    }
+
+    //----------------------------------------------------------------------------------------------
+
+    public void btnDesdeInternetClick ( View v ) {
+        // Reproducir un video desde internet
+        ruta = "http://secundaria.universoft.com.mx/clip_salvadas_en_la_linea.mp4";
+        lanzarVideoActiviy ();
+    }
+
+    //----------------------------------------------------------------------------------------------
+
+    public void btnDesdeAlmIntClick ( View v ) {
+        // Reproducir un video en la carpeta DCIM del almacenamiento interno
+        ruta = "file://" + Environment.getExternalStorageDirectory()
+                .getPath() + "/DCIM/la_pachanga.mp4";
+        lanzarVideoActiviy ();
+    }
+
+    //----------------------------------------------------------------------------------------------
+
+    private void  lanzarVideoActiviy () {
+        Intent intent = new Intent ( this, VideoActivity.class ) ;
+        intent.putExtra ( "rutaVideo", ruta );
+        startActivity ( intent );
+    }
+
+    //----------------------------------------------------------------------------------------------
+
 }
---
+++ U4VideoApp/app/src/main/java/mx/edu/itl/c18131209/u4videoapp/VideoActivity.java
@@ -0,0 +1,77 @@
+package mx.edu.itl.c18131209.u4videoapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class VideoActivity extends AppCompatActivity {
+
+    private VideoView videoView;
+    private String          ruta;
+    private ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+
+        // Preparar el progress Dialog
+        progressDialog = new ProgressDialog( this );
+        progressDialog.setTitle( "Reproduccion" );
+        progressDialog.setMessage( "Cargando..." );
+        progressDialog.setCanceledOnTouchOutside( false );
+        progressDialog.show();
+
+        videoView = findViewById( R.id.videoView );
+        ruta      = getIntent().getStringExtra( "rutaVideo" );
+
+        // Establecer el video que se va a reproducir y agregar los controles de reproduccion
+        videoView.setVideoURI(Uri.parse( ruta ) );
+        videoView.setMediaController( new MediaController( this ) );
+
+        // Al terminar la carga de video (preparado) se inicia la reproduccion
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp)
+            {
+                mp.setLooping( false );
+                videoView.requestFocus();
+                progressDialog.dismiss();
+                videoView.start();
+            }
+        });
+
+        // Al completar la reproduccion del video terminar la actividad
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                finish();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt ( "position", videoView.getCurrentPosition() );
+        videoView.pause();
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        int position = savedInstanceState.getInt( "position" );
+        videoView.seekTo( position );
+
+    }
+}
