e2f5b06c400e39189035cd9e6427f59065823ae4
Misael Adame <misael_adame@protonmail.com>
2021-05-17 17:37:19 -0500

    Aplicacion Date

--- MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView edtxtdFecha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtxtdFecha = findViewById(R.id.edtxtdFecha );
+
+        Date fechaActual = Calendar.getInstance().getTime();
+
+        edtxtdFecha.setText ( fechaActual.toString() );
+
     }
 }
