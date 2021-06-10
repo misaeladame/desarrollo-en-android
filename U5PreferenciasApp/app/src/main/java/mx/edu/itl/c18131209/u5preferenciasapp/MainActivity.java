65d3f58a87182c44363be91a9fc414906ef0d83f
Misael Adame <misael_adame@protonmail.com>
2021-06-10 16:38:37 -0500

    Preferencias App practicamente terminado

--- U5PreferenciasApp/app/src/main/java/mx/edu/itl/c18131209/u5preferenciasapp/MainActivity.java
+++ U5PreferenciasApp/app/src/main/java/mx/edu/itl/c18131209/u5preferenciasapp/MainActivity.java
@@ -1,11 +1,13 @@
 package mx.edu.itl.c18131209.u5preferenciasapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +30,23 @@ public class MainActivity extends AppCompatActivity {
 
         if ( pref != null  ) {
             boolean reprodMusica = pref.getBoolean ( "reproducir_musica", false );
+            int tipoGraficos = Integer.parseInt ( pref.getString ( "tipo_de_graficos", "100" ) );
+            int numFragmentos = Integer.parseInt ( pref.getString ( "numero_de_fragmentos", "44" ) );
+            boolean activarModoMultilple = pref.getBoolean ( "activar_modo_multiple", false );
+            String tonoAlarma = pref.getString ( "tono_de_alarma", "<ninguno>" );
+
+            AlertDialog.Builder builder = new AlertDialog.Builder ( this );
+            builder.setMessage ( "Reproducir musica: " + reprodMusica + "\n" +
+                    "Tipo de Graficos:  " + tipoGraficos + "\n" +
+                    "No. Fragmentos  :  " + numFragmentos + "\n" +
+                    "Activar Modo Mult: " + activarModoMultilple + "\n" +
+                    "Tono de alarma:    " + tonoAlarma
+            )
+                    .create()
+                    .show();
+
+        } else {
+            Toast.makeText ( this, "No se encontro el archivo de preferencias", Toast.LENGTH_LONG ).show ();
         }
 
     }
--- U5PreferenciasApp/app/src/main/java/mx/edu/itl/c18131209/u5preferenciasapp/PreferenciasActivity.java
+++ U5PreferenciasApp/app/src/main/java/mx/edu/itl/c18131209/u5preferenciasapp/PreferenciasActivity.java
@@ -2,13 +2,22 @@ package mx.edu.itl.c18131209.u5preferenciasapp;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 
 public class PreferenciasActivity extends PreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource ( R.xml.pantalla_preferencias );
+        // addPreferencesFromResource ( R.xml.pantalla_preferencias );
+        getFragmentManager().beginTransaction().replace ( android.R.id.content, new PreferenciasFragment () ).commit ();
+    }
 
+    public static class PreferenciasFragment extends PreferenceFragment {
+        @Override
+        public void onCreate ( final Bundle savedInstanceState ) {
+            super.onCreate ( savedInstanceState );
+            addPreferencesFromResource ( R.xml.pantalla_preferencias );
+        }
     }
 }
